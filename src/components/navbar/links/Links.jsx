261e9536edb39dc089b68436e136067002ab213c
mariemomar <113386463+mariemomar@users.noreply.github.com>
2024-08-13 08:28:32 +0300

    done navbar (without style)

---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+const Links = () => {
+  const links = [
+    {
+      title: "HomePage",
+      path: "/",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+    {
+        title: "Blog",
+        path: "/blog",
+      },
+  ];
+
+  return (
+    <div>
+      {links.map((link) => (
+        <Link key={link.title} href={link.path}>{link.title}</Link>
+      ))}
+    </div>
+  );
+};
+
+export default Links;
